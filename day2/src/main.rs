9f6ffe051b8e4c6010996e9fd224cebb972ad40c
Ayush0Chaudhary <ayush0000ayush@gmail.com>
2022-12-22 02:59:24 +0530

    day2 part2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,6 +1,6 @@
-use std::{fs::File, io::Read, iter::Skip};
+use std::{fs::File, io::Read};
 
-fn main() {
+fn main(){
     let mut file = File::open("/home/panda/Desktop/aoc/day2/src/data.txt").expect("sorry");
     let mut data = String::new();
     file.read_to_string(&mut data).expect("no transfer");
@@ -8,15 +8,15 @@ fn main() {
     for i in data.split("\n"){
         // xunwrap().to_string()
         datavec.push(i);
-
     }
+
     let mut sum  = 0 ;
     for i in 0..datavec.len(){
         let a = find_first(datavec.get(i));
         let b = find_second(datavec.get(i));
         let mut gg1 : rps1 = rps1::A;
         let mut gg2 : rps2 = rps2::X;
-        let mut gg3 = rps2::X;
+        let mut gg3 : rps2 = rps2::X;
         if a == "B" {
             gg1 = rps1::B;
         }
@@ -35,17 +35,13 @@ fn main() {
             gg3 = rps2::Z;
 
         }
+        let sc1 = f_sc(gg2);
+        let sc2 = s_sc(gg3, gg1);
         
-        // let ddc = rps1::a;
-        let sc1 = find_score(gg1, gg2);
-        
-        let sc2 = tool_score(gg3);
-        println!("{}", sc1+sc2);
         sum = sum +sc1+sc2;
     }
-    println!("{}",sum);
+    println!("{}", sum);
 }
-
 fn find_first(str : Option<&&str>) -> String{
     return str.unwrap().to_string().chars().nth(0).unwrap().to_string();
 }
@@ -53,42 +49,42 @@ fn find_second(str : Option<&&str>) -> String{
     return str.unwrap().to_string().chars().nth(2).unwrap().to_string();
 }
 
-fn tool_score (b :rps2) -> i32 {
-    match b {
-        rps2::Y =>  2,
-        rps2::Z =>  3,
-        rps2::X =>  1,
+fn f_sc(dig : rps2) -> i32{
+    match dig {
+        rps2::X => return 0,
+        rps2::Y => return 3,
+        rps2::Z => return 6,
     }
 }
 
-// A for Rock, B for Paper, and C for Scissors
-// X for Rock, Y for Paper, and Z for Scissors
-fn find_score(a:rps1, b :rps2) -> i32{
-    let mut answer = 0;
-    if a == rps1::A {
-        match b {
-            rps2::X =>  3,
-            rps2::Y =>  6,
-            rps2::Z =>  0,
-            
+fn s_sc(res : rps2, tool : rps1) -> i32 {
+    if res == rps2::X {
+        match tool {
+            rps1::A => return 3,
+            rps1::B => return 1,
+            rps1::C => return 2,
         }
     }
-    else if a == rps1::B {
-        match b {
-            rps2::X => return 0,
-            rps2::Y => return 3,
-            rps2::Z => return 6,
+    else if res == rps2::Y {
+        match tool {
+            rps1::A => return 1,
+            rps1::B => return 2,
+            rps1::C => return 3,
         }
     }
     else {
-        match b {
-            rps2::X => return 6,
-            rps2::Y => return 0,
-            rps2::Z => return 3,
+        match tool {
+            rps1::A => return 2,
+            rps1::B => return 3,
+            rps1::C => return 1,
         }
     }
-
+    
 }
+// A1 for Rock, B2 for Paper, and C3 for Scissors
+// X means lose, Y means draw, Z means  win
+// A for Rock, B for Paper, and C for Scissors
+
 #[derive(PartialEq)]
 enum rps1 {
     A,
@@ -101,4 +97,3 @@ enum rps2 {
     Y,
     Z,
 }
-

d9fbd0423cde4dffdd392b047fbd3e35f37b810a
Ayush0Chaudhary <ayush0000ayush@gmail.com>
2022-12-27 05:20:57 +0530

    day6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,25 +1,31 @@
-use std::{fs::File, io::Read};
-
-fn main() {
+use std::{fs::File, io::Read, collections::HashSet};
+fn main(){
     //file reading
     let mut file = File::open("/home/panda/Desktop/aoc/day6/src/data.txt").expect("sorry");
     let mut data = String::new();
     file.read_to_string(&mut data).expect("no transfer");
     let mut datavec = Vec::new();
     let mut sum =0;
-    
+
     for i in data.split("") {
         datavec.push(i);
         sum+=1;
-        if sum>4 {
-            let w1 = *datavec.get(sum-4).unwrap();
-            let w2 = *datavec.get(sum-3).unwrap();
-            let w3 = *datavec.get(sum-2).unwrap();
-            let w4 = *datavec.get(sum-1).unwrap();
-            if (w1 != w2) && (w1 != w3)&& (w1 != w4) && (w2 != w3) && (w2 !=w4 ) && (w3 != w4){
-                print!("{}", sum);
+        if sum>14 {
+            let mut vec = Vec::new();
+            for i in 0..14 {
+                vec.push(*datavec.get(sum- i -1).unwrap());
             }
+            // for i in vec{
+            //     print!("{}",i);
+            // }
+            // break;
+            println!("{}",vec.len());
+            if vec.iter().collect::<HashSet<_>>().len() == vec.len() {
+                print!("{}", sum);
+                break;
+            }
+            // vec.de
+            
         }
     }
-
 }
